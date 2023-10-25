a47c615817fdcbf730999396546e982ffbc264c1
wyffanny <144879490+wyffanny@users.noreply.github.com>
2023-10-25 15:01:42 +0800

    update

---
+++ 1025.r
@@ -0,0 +1,29 @@
+flights = readRDS("data/week7/flights.rds")
+
+flightsData <- flights$data[[1]]$data_frame
+
+dplyr::glimpse(flightsData)
+
+# summarise AirlineID
+levels(flightsData$AirlineID) |> length()
+
+sum(c(29, 23, 1, 17))
+
+c(29, 23, 1, 17) |> sum()
+
+c(1, 2, 2, 4, 6, 4, 6) |>
+  table()  
+
+flightsData$AirlineID |>
+  table() ->tb_airlineID 
+
+levels(flightsData$AirlineID)
+
+
+class(tb_airlineID)
+typeof(tb_airlineID)
+
+tb_airlineID |> sort(decreasing=TRUE)
+sort(tb_airlineID, decreasing=TRUE)
+
+sort(x)
